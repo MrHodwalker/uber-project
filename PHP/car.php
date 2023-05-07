935b7935a0ca1b9f83ba4db04c5977bc05cf4e09
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 18:15:35 -0500

    Added 4 new classes in PHP with class Car as parent.

--- PHP/car.php
+++ PHP/car.php
@@ -1,17 +1,18 @@
 <?php
-    class Car {
-        public $id;
-        public $license;
-        public $driver;
-        public $passenger;
+require_once('account.php');
+class Car {
+    public $id;
+    public $license;
+    public $driver;
+    public $passenger;
 
-        public function __construct($license, $driver) {
-            $this->license = $license;
-            $this->driver = $driver;
-        }
-
-        public function printDataCar() {
-            echo "License: $this->license, Driver: {$this->driver->name}, Document: {$this->driver->document} \n"; 
-        }
+    public function __construct($license, $driver) {
+        $this->license = $license;
+        $this->driver = $driver;
     }
+
+    public function printDataCar() {
+        echo "License: $this->license, Driver: {$this->driver->name}, Document: {$this->driver->document} \n"; 
+    }
+}
 ?>
--- PHP/payment.php
+++ PHP/payment.php
@@ -1,5 +1,5 @@
 <?php
-    class Payment {
-        public $id;
-    }
+class Payment {
+    public $id;
+}
 ?>
