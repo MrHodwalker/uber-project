77b43c593890299a60b53f9d5856344809053f00
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 14:02:45 -0500

    Created 2 objects and print some of the values in Java.

--- Java/Main.java
+++ Java/Main.java
@@ -1,5 +1,17 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        Car car = new Car();
+        car.license = "XYZ789";
+        car.driver = "Ernesto Cantillo";
+        car.passenger = 4;
+        car.printDataCar();
+
+        Car car2 = new Car();
+        car2.license = "ABC123";
+        car2.driver = "Jeff Bezos";
+        car2.passenger = 3;
+        car2.printDataCar();
     }
 }
