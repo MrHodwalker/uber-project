e8e5cc5a355ee7b13ffd9aba361707a922d2340d
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 14:10:55 -0500

    Created 2 objects and print their values in Python.

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,14 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hello World")
+
+    car = Car()
+    car.license = "XYZ789"
+    car.driver = "Ernesto Cantillo"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "ABC123"
+    car2.driver = "Jeff Bezos"
+    print(vars(car2))
