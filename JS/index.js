4c71b227ecd9b32334f63da4f647b3241bbd96e6
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 16:49:32 -0500

    Created 2 objects in JavaScript, changes to Account and Car in Java.

---
+++ JS/index.js
@@ -0,0 +1,7 @@
+var car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"))
+car.passenger = 4
+car.printDataCar()
+
+var car2 = new Car("ABC123", new Account("Jeff Bezos", "JB6789"))
+car2.passenger = 3
+car2.printDataCar()
--- Java/Main.java
+++ Java/Main.java
@@ -2,15 +2,11 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Car car = new Car();
-        car.license = "XYZ789";
-        car.driver = "Ernesto Cantillo";
+        Car car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"));
         car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "ABC123";
-        car2.driver = "Jeff Bezos";
+        Car car2 = new Car("ABC123", new Account("Jeff Bezos", "JB6789"));
         car2.passenger = 3;
         car2.printDataCar();
     }
