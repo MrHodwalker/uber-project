da584c8720b8889973bf563ee887c6ca3b82ac15
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 18:46:53 -0500

    Created 4 new classes in JavaScript using Car as parent.

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,10 @@ var car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"))
 car.passenger = 4
 car.printDataCar()
 
-var car2 = new Car("ABC123", new Account("Jeff Bezos", "JB6789"))
+/* var car2 = new Car("ABC123", new Account("Jeff Bezos", "JB6789"))
 car2.passenger = 3
-car2.printDataCar()
+car2.printDataCar() */
+
+var uberX = new UberX("ABC123", new Account("Jeff Bezos", "JB789"), "Kia", "Rio")
+uberX.passenger = 4
+uberX.printDataCar()
