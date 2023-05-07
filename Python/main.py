52a3b19ea74db1fb43934e0db6ae8f8b56649863
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 17:10:58 -0500

    Created 2 objects in Python and display their information.

--- Python/main.py
+++ Python/main.py
@@ -1,14 +1,13 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("Hello World")
 
-    car = Car()
-    car.license = "XYZ789"
-    car.driver = "Ernesto Cantillo"
+    car = Car("XYZ789", Account("Ernesto Cantillo", "EC1234"))
     print(vars(car))
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "ABC123"
-    car2.driver = "Jeff Bezos"
+    car2 = Car("ABC123", Account("Jeff Bezos", "JB6789"))
     print(vars(car2))
+    print(vars(car2.driver))
