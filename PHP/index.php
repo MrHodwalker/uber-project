e337c8cd6103d19b7106f0428174456c2fe55fbf
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 18:28:43 -0500

    Added 4 new classes in PHP with Car as parent print information about the classes.

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,17 @@
 <?php
     require_once('car.php');
+    require_once('uberX.php');
+    require_once('uberPool.php');
     require_once('account.php');
 
-    $car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"));
+    $uberX = new UberX("XYZ789", new Account("Ernesto Cantillo", "EC1234"), "Mazda", "CX-30");
+    $uberX->printDataCar();
+
+    $uberPool = new UberPool("ABC1234", new Account("Jeff Bezos", "JB6789"), "Renaul", "Logan");
+    $uberPool->printDataCar();
+
+    /* $car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"));
     $car->printDataCar();
 
     $car2 = new Car("ABC123", new Account("Jeff Bezos", "JB6789"));
-    $car2->printDataCar();
+    $car2->printDataCar(); */
