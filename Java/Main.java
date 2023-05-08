1f2a25f35ff40c0bf58069c2f80e90b6686234b5
MrHodwalker <mhodwalkerl@outlook.com>
2023-05-07 21:12:26 -0500

    Added encapsulation in Java.

--- Java/Main.java
+++ Java/Main.java
@@ -2,12 +2,16 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
 
-        Car car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"));
+        UberX uberX = new UberX("XYZ789", new Account("Ernesto Cantillo", "EC1234"), "Susuki", "Swift");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
+
+        /*Car car = new Car("XYZ789", new Account("Ernesto Cantillo", "EC1234"));
         car.passenger = 4;
         car.printDataCar();
 
         Car car2 = new Car("ABC123", new Account("Jeff Bezos", "JB6789"));
         car2.passenger = 3;
-        car2.printDataCar();
+        car2.printDataCar();*/
     }
 }
